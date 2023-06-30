8882a3951c7fa1e236171d9a1b42f95f4f5e3f9f
ezra <ezrayeneneh1992@gmail.com>
2023-06-30 12:43:14 +0300

    second django commit

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='freeze',
+    version='0.1',
+    description='A simple package that lists all of the Python packages that are currently installed in a virtual environment.',
+    url='https://github.com/bard/freeze',
+    author='Bard',
+    author_email='bard@example.com',
+    packages=['freeze'],
+    python_requires='>=3.7',
+)
