d3752f3e4fb82e10e76a2af90147f88705ed1001
ezra <ezrayeneneh1992@gmail.com>
2023-07-02 11:43:50 +0300

    really working

--- message-board/posts/tests.py
+++ message-board/posts/tests.py
@@ -3,10 +3,12 @@ from django.urls import reverse
 
 from .models import Post
 
+
 class PostTests(TestCase):
     @classmethod
     def setUpTestData(cls):
         cls.post = Post.objects.create(text="This is a test!")
+
     def test_model_content(self):
         self.assertEqual(self.post.text, "This is a test!")
 
