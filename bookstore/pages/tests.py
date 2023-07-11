ce088fa76669142a9909076cdd2559b4f78b6973
ezra <ezrayeneneh1992@gmail.com>
2023-07-11 08:26:43 +0300

    did crispy forms

--- bookstore/pages/tests.py
+++ bookstore/pages/tests.py
@@ -1,7 +1,7 @@
 from django.test import SimpleTestCase
 from django.urls import reverse, resolve
 
-from .views import HomePageView
+from .views import HomePageView, AboutPageView
 
 # Create your tests here.
 class HomePageTests(SimpleTestCase):
@@ -24,3 +24,20 @@ class HomePageTests(SimpleTestCase):
     def test_homepage_url_resolve_home_pageview(self):
         view = resolve("/")
         self.assertEqual(view.func.__name__, HomePageView.as_view().__name__)
+class AboutPageTests(SimpleTestCase):
+    def setUp(self):
+        url = reverse("about")
+        self.response = self.client.get(url)
+
+    def test_aboutpage_status_code(self):
+        self.assertEqual(self.response.status_code, 200)
+
+    def test_aboutpage_template(self):
+        self.assertTemplateUsed(self.response, "about.html")
+
+    def test_aboutpage_not_contains_correct_html(self):
+        self.assertNotContains(self.response, "About Page")
+
+    def test_aboutpage_url_resolve_aboutpageview(self):
+        view = resolve("/about/")
+        self.assertEqual(view.func.__name__, AboutPageView.as_view().__name__)
