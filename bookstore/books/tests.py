37e8398177fe6b5908743953e2c6f3ea9fdebf64
ezra <ezrayeneneh1992@gmail.com>
2023-07-12 10:40:59 +0300

    did all worked

--- bookstore/books/tests.py
+++ bookstore/books/tests.py
@@ -1,18 +1,29 @@
+from django.contrib.auth import get_user_model
 from django.test import TestCase
 from django.urls import reverse
 
-from .models import Book
+from .models import Book, Review
 
 # Create your tests here.
 class BookTests(TestCase):
 
     @classmethod
     def setUpTestData(cls):
+        cls.user = get_user_model().objects.create_user(
+                username="reviewuser",
+                email="reviewuser@email.com",
+                password="testpass123",
+            )
         cls.book = Book.objects.create(
                 title="Harry Potter",
                 author="JK Rowling",
                 price="25.00",
             )
+        cls.review = Review.objects.create(
+                book=cls.book,
+                author=cls.user,
+                review="An excellent review",
+        )
     def test_book_listiing(self):
         self.assertEqual(f"{self.book.title}", "Harry Potter")
         self.assertEqual(f"{self.book.author}", "JK Rowling")
