6516d8a886530bf5235f15c897c7aca91d38b21d
ezra <ezrayeneneh1992@gmail.com>
2023-07-15 09:03:55 +0300

    token worked successfully

--- blogapi/django_project/settings.py
+++ blogapi/django_project/settings.py
@@ -37,16 +37,28 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "django.contrib.sites", # new
+# 3rd-party apps
     "rest_framework",
     "corsheaders",
+    "rest_framework.authtoken",
+    "allauth", # new
+    "allauth.account", # new
+    "allauth.socialaccount", # new
+    "dj_rest_auth",
+    "dj_rest_auth.registration",
     "accounts.apps.AccountsConfig",
     "posts.apps.PostsConfig",
 ]
 
 REST_FRAMEWORK = {
-        "DEFAULT_PERMISSION_CLASSES": [
-            "rest_framework.permissions.IsAuthenticated",
+    "DEFAULT_PERMISSION_CLASSES": [
+        "rest_framework.permissions.IsAuthenticated",
         ],
+    "DEFAULT_AUTHENTICATION_CLASSES": [
+        "rest_framework.authentication.SessionAuthentication",
+        "rest_framework.authentication.TokenAuthentication"
+    ],
 }
 
 MIDDLEWARE = [
@@ -78,10 +90,13 @@ TEMPLATES = [
                 "django.template.context_processors.request",
                 "django.contrib.auth.context_processors.auth",
                 "django.contrib.messages.context_processors.messages",
+                "django.template.context_processors.request",
             ],
         },
     },
 ]
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+SITE_ID = 1
 
 WSGI_APPLICATION = "django_project.wsgi.application"
 
